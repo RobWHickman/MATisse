e4cc285f1a80448093a8cf64c93e01fd71df00c3
RobWHickman <robwhickman@gmail.com>
2017-12-05 17:12:08 +0000

    :dizzy: fixed unneat signalling of task in load stimuli

--- src/Generate_Task/load_stimuli.m
+++ src/Generate_Task/load_stimuli.m
@@ -8,11 +8,11 @@ stimuli.settings.bidspace_overhang = 20;
 %load the fractals
 %stimuli settings used for the file path and screen_info used to resize the
 %fractals
-stimuli.fractals = load_fractal_images(stimuli.settings, hardware.outputs.screen_info, task);
+stimuli.fractals = load_fractal_images(stimuli.settings, hardware.outputs.screen_info, parameters.task);
 %generate the bidspace
 %the bar is also considered part of the bidspace for ease of storing
 %variables
-stimuli.bidspace = generate_bidspace(stimuli.settings, hardware.outputs.screen_info, task_window, task);
+stimuli.bidspace = generate_bidspace(stimuli.settings, hardware.outputs.screen_info, task_window, parameters.task);
 %generate a fixation cross
 %get rid of magic numbers which correspond to:
 %length, thickness, colour and surrounding box scalar
