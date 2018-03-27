768bb51fe8a4293d1bf44b3ceb618ffc6abf272b
RobWHickman <robwhickman@gmail.com>
2018-03-27 14:46:41 +0100

    reinitialised rewriting

---
+++ analysis/convert_2_csv.m
@@ -0,0 +1,29 @@
+function [] = convert_2_csv(filename)
+input_folder = 'C:/Users/WS-Guest/Desktop/task data/';
+output_folder = [input_folder, 'joystick_vectors/'];
+
+input_filename = [input_folder, filename, '.mat'];
+load(input_filename);
+
+bidding_vector_output = results.full_output_table.trial_values.bidding_vector;
+trials = length(bidding_vector_output);
+
+for trial = 1:trials
+    joystick_vector = bidding_vector_output{trial};
+    length_vector = length(joystick_vector);
+    
+    missing_pad = NaN(1, 300 - length_vector);
+    
+    output_vector = [joystick_vector, missing_pad];
+
+    if trial == 1
+        output_matrix = output_vector;
+    else
+        output_matrix = vertcat(output_matrix, output_vector);
+    end
+end
+
+output_file = [output_folder, filename, '.csv'];
+csvwrite(output_file, output_matrix);
+end
+
