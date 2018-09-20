f84d041a805d6ba8ce750b9377de612091360919
RobWHickman <RobWHickman@gmail.com>
2018-09-20 18:49:44 +0100

    removed breaking daqreset from find_solenoid()

--- src/get_devices/find_solenoid.m
+++ src/get_devices/find_solenoid.m
@@ -1,7 +1,7 @@
 %function to grab the solenoid given the set up in 313
 %doesnt maintain the handshake stuff- see Modig
 function hardware = find_solenoid(hardware)
-daqreset
+
 solenoid = daq.createSession('ni');
 addDigitalChannel(solenoid,'Dev1','Port0/Line9:11','OutputOnly');
 % tap_open = [0 1 0]; %tap1 water
